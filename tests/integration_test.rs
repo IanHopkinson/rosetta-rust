7198a1c493716660ff1bd84f0af5ee001211a83a
Ian Hopkinson <ianhopkinson@googlemail.com>
2025-01-05 07:53:57 +0000

    Add an integration test

---
+++ tests/integration_test.rs
@@ -0,0 +1,7 @@
+use wordcount_lib;
+
+#[test]
+fn test_wordcount() {
+    let file_path = String::from("tests/fixtures/word_count_test_file.txt");
+    assert_eq!(wordcount_lib::run(&file_path), 6);
+}
